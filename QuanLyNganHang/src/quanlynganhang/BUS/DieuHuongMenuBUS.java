eaefb65b5293074b6e8d27920939e07dc6806d14
thaiduong03122003 <duongthai3122003@gmail.com>
2024-04-10 00:40:12 +0700

    Update UI Part 3

--- QuanLyNganHang/src/quanlynganhang/BUS/DieuHuongMenuBUS.java
+++ QuanLyNganHang/src/quanlynganhang/BUS/DieuHuongMenuBUS.java
@@ -2,6 +2,9 @@ package quanlynganhang.BUS;
 
 import quanlynganhang.GUI.Application;
 import quanlynganhang.GUI.FormDSKhachHang;
+import quanlynganhang.GUI.FormMoTaiKhoan;
+import quanlynganhang.GUI.FormMoTheGhiNo;
+import quanlynganhang.GUI.FormMoTheTinDung;
 import quanlynganhang.GUI.FormThongKe;
 import quanlynganhang.GUI.FormTongQuan;
 import quanlynganhang.GUI.FormTrangChu;
@@ -25,11 +28,13 @@ public class DieuHuongMenuBUS {
                 Application.showForm(new FormTongQuan());
             } else if (index == 2) {
                 Application.showForm(new FormDSKhachHang());
-            } else if (index == 3) {
+            } else if (index == 6) {
+                Application.showForm(new FormMoTaiKhoan());
+            } else if (index == 7) {
                 if (subIndex == 1) {
-                    //Application.showForm(new FormThongKe());
+                    Application.showForm(new FormMoTheGhiNo());
                 } else if (subIndex == 2) {
-                    //Application.showForm(new FormRead());
+                    Application.showForm(new FormMoTheTinDung());
                 } else {
                     action.cancel();
                 }
